3feba1fed32b42d06f994af65839e5269daeec88
xifang <xifang1102@hotmail.com>
2023-12-11 16:30:23 +0800

    13.92

--- cpp/src/arrow/io/hdfs_internal.cc
+++ cpp/src/arrow/io/hdfs_internal.cc
@@ -341,16 +341,16 @@ Status ConnectLibHdfs(LibHdfsShim** driver) {
 
     shim->Initialize();
 
-    ARROW_ASSIGN_OR_RAISE(auto libjvm_potential_paths, get_potential_libjvm_paths());
-    ARROW_ASSIGN_OR_RAISE(libjvm_handle, try_dlopen(libjvm_potential_paths, "libjvm"));
+    // ARROW_ASSIGN_OR_RAISE(auto libjvm_potential_paths, get_potential_libjvm_paths());
+    // ARROW_ASSIGN_OR_RAISE(libjvm_handle, try_dlopen(libjvm_potential_paths, "libjvm"));
 
-    /*
+
     std::string yourPath = "/home/xifang/arrow-parquet-test/libhdfs/jdk1.8.0_381/jre/lib/aarch64/server/libjvm.so";
     auto pf = PlatformFilename::FromString(yourPath));
     std::vector<PlatformFilename> myPaths;
     myPaths.push_back(pf);
     ARROW_ASSIGN_OR_RAISE(libjvm_handle, try_dlopen(myPaths, "libjvm"));
-     */
+
 
     ARROW_ASSIGN_OR_RAISE(auto libhdfs_potential_paths, get_potential_libhdfs_paths());
     ARROW_ASSIGN_OR_RAISE(shim->handle, try_dlopen(libhdfs_potential_paths, "libhdfs"));
